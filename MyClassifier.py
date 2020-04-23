65a100a74299c41e11964b2332fabcec20ab24e4
Chad Gavin Sia Si Cong <csia5929@uni.sydney.edu.au>
2020-04-23 19:48:06 +0800

    data input done, changed string to float

--- MyClassifier.py
+++ MyClassifier.py
@@ -3,35 +3,44 @@ import numpy as np
 import math
 import sys
 
+def convert(x):
+    try:
+        return float(x)
+    except ValueError:
+        return x.title()
+
 def extract(input_file):
     data = []
     lines = []
+    data_type =[]
     with open(input_file,'r') as f:
         lines = f.readlines()
     lines = [x.strip() for x in lines]
     for i in lines:
         data.append(i.split(","))
-    
-    changing_to_float(data) 
+    data = [[convert(element) for element in entry] for entry in data]
+
     return data
-def changing_to_float(data):
-    for i in data:
-        for j in range(len(i)):
-            if j != 'yes' and j!= 'no':
-                i[j] = np.float(i[j])
-
-
-
+# def NB(training_input,tesing_input):
+    
+# def KNN(k,training_data,tesing_input):
+   
 def main(argv):
 	print('a')
 
 if __name__ == "__main__":
-   df =[]
-   training_data = sys.argv[1]
-   testing_data = sys.argv[2]
-   algorithm = sys.argv[3]
-   training_input = extract(training_data)
-   #tesing_input = extract(testing_data)
-   print(training_input)
-   '''if algorithm == 'NB':
-       NB(traing)'''
+    results =[]
+    training_data = sys.argv[1]
+    testing_data = sys.argv[2]
+    algorithm = sys.argv[3]
+    training_input = extract(training_data)
+    tesing_input = extract(testing_data)
+    
+    print(training_input)
+    if algorithm == 'NB':
+       result = NB(training_input,tesing_input)
+    elif 'NN' in algorithm:
+       k = int(algorithm.strip("NN"))
+       for i in tesing_input:
+           result.append(KNN(k,training_data,tesing_input))
+
